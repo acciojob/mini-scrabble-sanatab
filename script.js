317e87faa8e7ee04990629e0ac8d4d8e86931bf2
sanatab <120128180+sanatab@users.noreply.github.com>
2023-01-22 02:49:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your code here
+let evaluatedText = document.getElementById("evaluatedText");
+let letterCount = document.getElementById("letterCount");
+
+evaluatedText.addEventListener("input", countWord)
+
+function countWord(){
+   let word = evaluatedText.value;
+   let count = word.length;
+   letterCount.innerHTML = count;
+}
